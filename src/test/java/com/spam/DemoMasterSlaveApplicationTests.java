f7e01d918bd165b8d3daf3838e38e9a8dbde27c7
Tran Dinh Hoan <hoant0355@gmail.com>
2025-12-16 15:44:16 +0700

    inti

---
+++ src/test/java/com/spam/DemoMasterSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMasterSlaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
